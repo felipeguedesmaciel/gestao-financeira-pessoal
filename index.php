2525bb241d3c1b24eb6e1a8ce1e0012144a5a01c
felipe1000 <felipeguedes1000@gmail.com>
2024-02-08 23:05:28 -0300

    Criando o Dashboard de gestão com HTML

--- index.php
+++ index.php
@@ -6,6 +6,96 @@
     <title>Gestão Financeira Pessoal</title>
 </head>
 <body>
-    <h1>Gestão Financeira Pessoal</h1>
+    <header>
+        <h1>Gestão Financeira Pessoal</h1>
+        <p>Previsão: <span>on/of</span></p>
+        <nav>
+
+        </nav>
+    </header>
+    <main>
+        <section>
+            <h2>Saldo total: R$0.000,00</h2>
+        </section>
+        <section>
+            <h2>Este Mês</h2>
+            <div>Entradas<p>R$0.00</p></div>
+            <div>Saídas<p>R$0.00</p></div>
+            <div>Saldo<p>R$0.00</p></div>
+            <div>
+                <h3>Categorias Inseridas</h3>
+                <div class="categorias">
+                    <div class="item-categoria">
+                        <p>Casa</p><p>R$0.00</p>
+                    </div>
+                    <div class="item-categoria">
+                        <p>Carro</p><p>R$0.00</p>
+                    </div>
+                    <div class="item-categoria">
+                        <p>Mercado</p><p>R$0.00</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <h2>Próximo Mês:</h2>
+            <div>Entradas<p>R$0.00</p></div>
+            <div>Saídas<p>R$0.00</p></div>
+            <div>Saldo<p>R$0.00</p></div>
+            <div>
+                <h3>Categorias Inseridas</h3>
+                <div class="categorias">
+                    <div class="item-categoria">
+                        <p>Casa</p><p>R$0.00</p>
+                    </div>
+                    <div class="item-categoria">
+                        <p>Carro</p><p>R$0.00</p>
+                    </div>
+                    <div class="item-categoria">
+                        <p>Mercado</p><p>R$0.00</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <h2>Ano:2023</h2>
+            <div>
+                <h3>Categorias</h3>
+                <div class="categorias">
+                    <div class="item-categoria">
+                        <p>Casa</p><p>R$0.00</p>
+                    </div>
+                    <div class="item-categoria">
+                        <p>Carro</p><p>R$0.00</p>
+                    </div>
+                    <div class="item-categoria">
+                        <p>Mercado</p><p>R$0.00</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <h2>Reserva de Emergência</h2>
+            <table>
+                <tr>
+                    <td>Valor Depositado:</td>
+                    <td>R$0.00</td>
+                </tr>
+                <tr>
+                    <td>Meta de Valor:</td>
+                    <td>R$0.00</td>
+                </tr>
+                <tr>
+                    <td>Ainda Falta</td>
+                    <td>R$0.00</td>
+                </tr>
+            </table>
+        </section>
+        <section>
+            <h2>Reserva de Oportunidades</h2>
+            <p>Valor Depositado: R$0.00</p>
+        </section>
+    </main>
+    <footer></footer>
 </body>
 </html>
