2bd24a55f6d8e2b899854febfff973422602729a
felipe1000 <felipeguedes1000@gmail.com>
2024-02-14 00:38:34 -0300

    Criação do painel.php e inicio de css

--- index.php
+++ index.php
@@ -6,147 +6,19 @@
     <title>Gestão Financeira Pessoal</title>
 </head>
 <body>
-    <header>
-        <h1>Gestão Financeira Pessoal</h1>
-        <p>Previsão: <span>on/of</span></p>
-        <nav>
-
-        </nav>
-    </header>
-    <main>
-        <section>
-            <h2>Saldo total: R$0.000,00</h2>
-        </section>
-        <section>
-            <h2>Este Mês</h2>
-            <div>Entradas<p>R$0.00</p></div>
-            <div>Saídas<p>R$0.00</p></div>
-            <div>Saldo<p>R$0.00</p></div>
-            <div>
-                <h3>Categorias Inseridas</h3>
-                <div class="categorias">
-                    <div class="item-categoria">
-                        <p>Casa</p><p>R$0.00</p>
-                    </div>
-                    <div class="item-categoria">
-                        <p>Carro</p><p>R$0.00</p>
-                    </div>
-                    <div class="item-categoria">
-                        <p>Mercado</p><p>R$0.00</p>
-                    </div>
-                    <div class="item-categoria">
-                        <p>Internet</p><p>R$0.00</p>
-                    </div>
-                    <div class="item-categoria">
-                        <p>Outros</p><p>R$0.00</p>
-                    </div>
-                    <div class="item-categoria">
-                        <p>Viagens</p><p>R$0.00</p>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <h3>Próximos Vencimento</h3>
-                <table style="border: solid 1px black;">
-                    <thead>
-                        <tr>
-                            <th>Categoria</th>
-                            <th>Vencimento</th>
-                            <th>Valor</th>
-                        </tr>
-                    </thead>
-                    <tr>
-                        <td>Carro</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr class="butao">
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </table>
-            </div>
-        </section>
-        <section>
-            <h2>Próximo Mês:</h2>
-            <div>Entradas<p>R$0.00</p></div>
-            <div>Saídas<p>R$0.00</p></div>
-            <div>Saldo<p>R$0.00</p></div>
-            <div>
-                <h3>Categorias Inseridas</h3>
-                <div class="categorias">
-                    <div class="item-categoria">
-                        <p>Casa</p><p>R$0.00</p>
-                    </div>
-                    <div class="item-categoria">
-                        <p>Carro</p><p>R$0.00</p>
-                    </div>
-                    <div class="item-categoria">
-                        <p>Mercado</p><p>R$0.00</p>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section>
-            <h2>Ano:2023</h2>
-            <div>
-                <h3>Categorias</h3>
-                <div class="categorias">
-                    <div class="item-categoria">
-                        <p>Casa</p><p>R$0.00</p>
-                    </div>
-                    <div class="item-categoria">
-                        <p>Carro</p><p>R$0.00</p>
-                    </div>
-                    <div class="item-categoria">
-                        <p>Mercado</p><p>R$0.00</p>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section>
-            <h2>Reserva de Emergência</h2>
-            <table>
-                <tr>
-                    <td>Valor Depositado:</td>
-                    <td>R$0.00</td>
-                </tr>
-                <tr>
-                    <td>Meta de Valor:</td>
-                    <td>R$0.00</td>
-                </tr>
-                <tr>
-                    <td>Ainda Falta</td>
-                    <td>R$0.00</td>
-                </tr>
-            </table>
-        </section>
-        <section>
-            <h2>Reserva de Oportunidades</h2>
-            <p>Valor Depositado: R$0.00</p>
-        </section>
-    </main>
-    <footer></footer>
+    <h1>Gestão Financeira Pessoal</h1>
+    <div>
+        <form action="painel.php">
+            <p>
+                <label for="iLogin">Login</label>
+                <input type="text" name="Login" id="iLogin">
+            </p>
+            <p>
+                <label for="iSenha">Senha</label>
+                <input type="password" name="Senha" id="iSenha">
+            </p>
+            <p><input type="submit" value="Entrar"></p>
+        </form>
+    </div>
 </body>
 </html>
